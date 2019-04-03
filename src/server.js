8caca33bad717b4d88cd28fab1b07ac2bef398eb
Sajani Patel <spatel@nwed.esrd.net>
2019-04-03 11:34:17 -0400

    heroku changes 2

--- src/server.js
+++ src/server.js
@@ -5,12 +5,12 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist/quicksight-poc'));
+app.use(express.static(__dirname + '/dist/quicksight-poc/src'));
 
 app.get('/*', function(req,res) {
 
-  res.sendFile(path.join(__dirname+'/dist/quicksight-poc/index.html'));
+  res.sendFile(path.join(__dirname+'/dist/quicksight-poc/src/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
-app.listen(process.env.PORT || 4200);
+app.listen(process.env.PORT || 8080);
